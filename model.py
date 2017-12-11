b698889fb9ce727cfef442608e4a9dcd91e16037
Radu Potop <radu@myrecovery.ai>
2017-12-11 15:18:03 +0000

    updated model

--- model.py
+++ model.py
@@ -4,23 +4,23 @@ import peewee as pw
 db = pw.SqliteDatabase('msgs.db')
 
 
-class MsgLog(pw.Model):
+class MsgMeta(pw.Model):
     date = pw.DateTimeField()
     from = pw.CharField()
     to = pw.CharField()
     subject = pw.CharField()
     has_attachment = pw.BooleanField()
-    fetch_time = pw.DateTimeField(default=datetime.datetime.now)
-    imap_uid = pw.CharField(index=True)
-    csum = pw.CharField(unique=True, index=True)
+    csum = pw.ForeignKeyField(RawMsg, to_field='csum')
 
     class Meta:
         database = db
 
 
 class RawMsg(pw.Model):
-    csum = pw.ForeignKeyField(MsgLog, to_field='csum')
     email_blob = pw.BlobField()
+    csum = pw.CharField(unique=True, index=True)
+    imap_uid = pw.CharField(index=True)
+    fetch_time = pw.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
