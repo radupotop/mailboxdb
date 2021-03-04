5ea258b5a69a9d4c5aaa9b11d46f8a95fb189b5f
Radu Potop <radu@wooptoo.com>
2021-03-04 23:45:26 +0000

    Add email multipart generator

--- tests/composeemail.py
+++ tests/composeemail.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
 
 import logging
-
+import random
+from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from pathlib import Path
 
 logging.basicConfig()
 log = logging.getLogger('compose-email')
 log.setLevel(logging.INFO)
 
+LOREM_IMPSUM_PATH = 'tests/fixtures/lorem_ipsum.txt'
+LF = '\n'
 
-def compose_email(html_body):
+
+def read_lipsum():
+    lipsum = Path(LOREM_IMPSUM_PATH)
+    lipsum_list = lipsum.read_text().split(LF)
+    rnd_line = random.randint(0, len(lipsum_list) - 1)
+    return lipsum_list[rnd_line]
+
+
+def compose_email():
     """
     Compose an email with MIME type multipart/alternative.
     """
+    now = datetime.utcnow()
     msg = MIMEMultipart('alternative')
 
-    msg['Subject'] = 'Test email 1'
-    msg['From'] = 'test1@example.org'
-    msg['To'] = 'test@example.org'
+    msg['Subject'] = f'Test Email {now}'
+    msg['From'] = 'test@localhost'
+    msg['To'] = 'test2@localhost'
 
-    text = 'This is the plain text part of the email.'
-    html_body = '<p>html email</p>'
+    text = read_lipsum()
+    html_body = f'<strong><em>{text}</em></strong>'
 
     part1 = MIMEText(text, 'plain')
     part2 = MIMEText(html_body, 'html')
 
     msg.attach(part1)
     msg.attach(part2)
-
-    log.info('Msg body: ')
-    log.info(msg)
 
     return msg
