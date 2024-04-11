36b164889ce3f58cf3b458304680367d9bd400b2
Radu Potop <radu@wooptoo.com>
2024-04-11 20:06:09 +0100

    Fixes for setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
-setup(name='Mailboxdb', version='0.2.2', packages=find_packages())
+
+def read_requirements(filename) -> list:
+    reqlist = Path(filename).read_text().splitlines()
+    return list(filter(lambda line: '==' in line, reqlist))
+
+
+setup(
+    name='Mailboxdb',
+    version='0.2.2',
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+    # extras_require=dict(tests=read_requirements('requirements.test.txt')),
+)
